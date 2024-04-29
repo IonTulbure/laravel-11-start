5aa71dba72141b0611e9f566ae1b33bb2130d2d7
IonTulbure <iontulburedev@gmail.com>
2024-04-29 17:16:00 +0300

    feat: basic route for custom welcome blade

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+// Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+Route::get('/', [WelcomeController::class, 'customWelcome'])->name('customWelcome');
