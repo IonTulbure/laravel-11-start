fb0c43cfcb13ee530919e5cf7009202fa719ec08
IonTulbure <iontulburedev@gmail.com>
2024-04-30 12:10:50 +0300

    add distinct html for Note views

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+    <h1>Index Note</h1>
 </div>
--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+    <h1>Show Note</h1>
 </div>
