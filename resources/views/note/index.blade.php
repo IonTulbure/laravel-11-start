e4271f97c1b9dd999a05f3434d0708eda85dc626
IonTulbure <iontulburedev@gmail.com>
2024-05-02 08:52:06 +0300

    set layout for Note index page

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,3 +1,21 @@
 <x-layout>
-    <h1>Index Note</h1>
+    <div class="note-container">
+        <a href="#" class="new-note-btn">
+            New note
+        </a><!-- new-note-btn -->
+        <div class="notes">
+            @foreach ($notes as $note)
+            <div class="note">
+                <div class="note-body">
+                    {{ $note->note }}
+                </div><!-- note-body -->
+                <div class="note-buttons">
+                    <a href="#" class="note-edit-button">View</a><!-- note-edit-button -->
+                    <a href="#" class="note-edit-button">Edit</a><!-- note-edit-button -->
+                    <button class="note-delete-button">Delete</button><!-- note-delete-button -->
+                </div><!-- note-buttons -->
+            </div><!-- note -->
+            @endforeach
+        </div><!-- notes -->
+    </div><!-- note-container -->
 </x-layout>
