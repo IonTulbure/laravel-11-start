a2da1d8e45082ae34aae9a4f31d73b516ffeb096
IonTulbure <iontulburedev@gmail.com>
2024-05-02 09:23:26 +0300

    include css & js with vite server

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="antialiased">
